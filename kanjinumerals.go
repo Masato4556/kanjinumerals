a89c1378157a32026cb9fe24579d8844e29ee42a
Otsuka Masato <ootsukamase@gmail.com>
2022-07-30 01:08:09 +0900

    update: 4桁を表す漢数字をintに変換するロジックを追加

--- kanjinumerals.go
+++ kanjinumerals.go
@@ -2,14 +2,56 @@ package kanjinumerals
 
 import (
 	"log"
+	"math"
 	"strings"
 )
 
-var LargeNumeralSymbols = []string{"万", "億", "兆"}
+var LargeNumeralSymbols = map[string]int{"万": 4, "億": 8, "兆": 12}
+var MediumNumeralSymbols = map[string]int{"十": 1, "百": 2, "千": 3}
+var SmallNumeralSymbols = map[string]int{"〇": 0, "一": 1, "二": 2, "三": 3, "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9}
 
 type FourDigitKanji struct {
 	V []string // 4桁の数値
 	E string   // 10の冪乗
+}
+
+func (k FourDigitKanji) ToFourDigitNumber() (n FourDigitNumber) {
+	n = FourDigitNumber{V: k.numberV(), E: 0}
+	if e, ok := LargeNumeralSymbols[k.E]; ok {
+		// TODO: Vの値を計算する
+		n.E = e
+	}
+	return
+}
+
+func (k FourDigitKanji) numberV() (nv int) {
+	temp := 0
+	digits := []int{}
+	for i, v := range k.V {
+		if mns, ok := MediumNumeralSymbols[v]; ok {
+			if i == 0 {
+				temp = 1
+			}
+			temp *= int(math.Pow10(mns))
+			digits = append(digits, temp)
+			temp = 0
+		}
+		if sns, ok := SmallNumeralSymbols[v]; ok {
+			temp += sns
+		}
+	}
+	if temp != 0 {
+		digits = append(digits, temp)
+	}
+	for _, v := range digits {
+		nv += v
+	}
+	return nv
+}
+
+type FourDigitNumber struct {
+	V int // 4桁の数値
+	E int // 10の冪乗
 }
 
 // splitNumeralSymbols 数詞で分割
@@ -21,7 +63,7 @@ func splitNumeralSymbols(s string) []string {
 func splitToFourDigit(kanjiNumeralSymbols []string) (fourDigitKanji []FourDigitKanji) {
 	stuck := []string{}
 	for _, v := range kanjiNumeralSymbols {
-		if contains(LargeNumeralSymbols, v) {
+		if _, ok := LargeNumeralSymbols[v]; ok {
 			fourDigitKanji = append(fourDigitKanji, FourDigitKanji{V: stuck, E: v})
 			stuck = []string{}
 		} else {
--- kanjinumerals_test.go
+++ kanjinumerals_test.go
@@ -86,3 +86,84 @@ func Test_splitToFourDigit(t *testing.T) {
 		})
 	}
 }
+
+func TestFourDigitKanji_ToFourDigitNumber(t *testing.T) {
+	type fields struct {
+		V []string
+		E string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		wantN  FourDigitNumber
+	}{
+		{
+			name:   "十二兆",
+			fields: fields{V: []string{"十", "二"}, E: "兆"},
+			wantN:  FourDigitNumber{V: 12, E: 12},
+		},
+		{
+			name:   "百二億",
+			fields: fields{V: []string{"百", "二"}, E: "億"},
+			wantN:  FourDigitNumber{V: 102, E: 8},
+		},
+		{
+			name:   "二千九百五十七万",
+			fields: fields{V: []string{"二", "千", "九", "百", "五", "十", "七"}, E: "万"},
+			wantN:  FourDigitNumber{V: 2957, E: 4},
+		},
+		{
+			name:   "二千九百五十七万",
+			fields: fields{V: []string{"六", "千", "五", "十"}, E: ""},
+			wantN:  FourDigitNumber{V: 6050, E: 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := FourDigitKanji{
+				V: tt.fields.V,
+				E: tt.fields.E,
+			}
+			if gotN := k.ToFourDigitNumber(); !reflect.DeepEqual(gotN, tt.wantN) {
+				t.Errorf("FourDigitKanji.ToFourDigitNumber() = %v, want %v", gotN, tt.wantN)
+			}
+		})
+	}
+}
+
+func TestFourDigitKanji_numberV(t *testing.T) {
+	type fields struct {
+		V []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		wantNv int
+	}{
+		{
+			name:   "",
+			fields: fields{V: []string{"十", "二"}},
+			wantNv: 12,
+		},
+		{
+			name:   "",
+			fields: fields{V: []string{"百", "二"}},
+			wantNv: 102,
+		},
+		{
+			name:   "",
+			fields: fields{V: []string{"二", "千", "九", "百", "五", "十", "七"}},
+			wantNv: 2957,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := FourDigitKanji{
+				V: tt.fields.V,
+			}
+			if gotNv := k.numberV(); gotNv != tt.wantNv {
+				t.Errorf("FourDigitKanji.numberV() = %v, want %v", gotNv, tt.wantNv)
+			}
+		})
+	}
+}
